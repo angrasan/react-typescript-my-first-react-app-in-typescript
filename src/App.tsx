55a33072143a93ddee615709240e6695207908ba
angrasan <a0n9g2r6a@outlook.jp>
2022-05-30 21:07:12 +0900

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import Counter from "./Counter";
 
 interface AppProps {
   message?: string;
@@ -6,13 +7,17 @@ interface AppProps {
 
 // type NulAppProps = Partial<AppProps>
 
-const App: React.FunctionComponent<AppProps> = ({message}) => {
-  return <div>{message}</div>;
+const App: React.FunctionComponent<AppProps> = ({ message }) => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 };
 
 App.defaultProps = {
-  message: 'Hello defaultProps!',
+  message: "Hello defaultProps!",
   // description: 'This is App component',
-}
+};
 
 export default App;
